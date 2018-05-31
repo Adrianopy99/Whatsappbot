70d66cc706e432c5934722d78e3eb661a416f1b1
Adrianopy99 <39531454+Adrianopy99@users.noreply.github.com>
2018-05-31 13:41:10 -0300

    whatsappbot 2.0

--- script.py
+++ script.py
@@ -2,6 +2,9 @@ import pyautogui
 import time
 
 time.sleep(5)
+pyautogui.moveTo(798,733)
+pyautogui.click()
+time.sleep(3)
 pyautogui.typewrite("Hello World !",interval=0.25)
 time.sleep(2)
 pyautogui.press("enter")
